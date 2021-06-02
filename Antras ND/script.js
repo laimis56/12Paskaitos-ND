47bf94dc2391c59501febf6e87d74ce5ba13ca05
laimis56 <83597616+laimis56@users.noreply.github.com>
2021-06-02 18:20:22 +0100

    Add files via upload

---
+++ Antras ND/script.js
@@ -0,0 +1,4 @@
+
+var names = ["Jon Snow", "Cersei Lannister", "Daenerys Targaryen", "Theon Greyjoy", "Tyrion Lannister", "Arya Stark"];
+
+console.log("Personazas: " + names[2]);
---
+++ Pirmas ND/script.js
@@ -0,0 +1,8 @@
+
+
+
+
+
+let a = prompt("Kaina su PVM", 121);
+let b = prompt("PVM", 21);
+let c = prompt("Kaina be PVM",100);
